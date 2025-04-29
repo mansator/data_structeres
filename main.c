1175db6c56a15d4edf42b450dacdc79b133edaad
Tolstenev Gleb <tolstenev.gleb@gmail.com>
2025-04-29 18:24:47 +0000

    Начали работу над IntArray

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct IntArray {
+  int *array;
+  int size;
+  int capacity;
+} IntArray;
+
+void print_array(IntArray a) {
+  for (int i = 0; i < a.size; i++) {
+    printf("Element[%d]: %d\n", i, a.array[i]);
+  } 
+}
+
+int main() {
+  IntArray temperature = {0};  // Массив значений температуры за период
+  printf("Enter the size: ");
+  scanf("%d", &temperature.size);
+
+  temperature.array = (int *)malloc(temperature.size * sizeof(int));
+  if (temperature.array != NULL) {
+    for (int i = 0; i < temperature.size; i++) {
+      printf("Enter the element[%d]: ", i);
+      scanf("%d", &temperature.array[i]);
+    }
+    
+    print_array(temperature);
+    temperature.array[temperature.size - 1] = 0;
+    temperature.size -= 1;
+    printf("\n");
+    print_array(temperature);
+  }
+  return 0;
+}
